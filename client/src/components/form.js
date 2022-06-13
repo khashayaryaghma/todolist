3b8a7e9bea2f0dd257442a5d9a31680513239f44
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-13 21:33:26 +0430

    Update form.js

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -5,13 +5,13 @@ import TextInput from "./textInput";
 export default function Form() {
     const [list, setlist] = useState({text:""});
     function formSubmit(e){
-        
+
     }
     return (
         <>
             <form onSubmit={formSubmit} className="input-group mt-5" >
                     <TextInput />
-                    <ActionButton color="primary" size="sm" value="Submit" />
+                    <ActionButton color="primary" size="sm" value="Submit" type="submit" />
             </form>
         </>
     );
