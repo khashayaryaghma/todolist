c3a0125489663175f8120c033c386c85da0b264c
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-14 20:49:58 +0430

    Update lists.js

--- client/src/components/lists.js
+++ client/src/components/lists.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Lists({listData}) {
 
   return (
-    <div>
+    <>
         <ul>
             {listData?.map((text)=>{
                 return <li>{text}</li>
             })}
         </ul>
-    </div>
+    </>
   )
 }
