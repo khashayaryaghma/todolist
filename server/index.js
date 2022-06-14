ed375ca5c892d1fd94d418244030c62403daff4a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-14 18:55:50 +0430

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 5000;
-const listRouter = require("./routes/list.routes")
+const express = require('express');
+const dotenv = require("dotenv");
+const connectDB = require("./db/connect");
 
-app.use("/list",listRouter)
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+dotenv.config();
+const app = express()
+
+// const listRouter = require("./routes/list.routes")
+
+// app.use("/list",listRouter);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// const PORT = 5000;
+// app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
