4f2b86ff9411db6bf6ade4f2b182ecec9271bd81
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-14 19:00:50 +0430

    schema

---
+++ server/model/lists.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const listSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, "must provide course title"],
+        maxlength: [20, "course must not be more than 20 characters"],
+    },
+});
+
+module.exports = mongoose.model("list", listSchema);
